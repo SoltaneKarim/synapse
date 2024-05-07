038cf3c0b0fac1b27bb4bdeeb8a5efd7ee7360a7
SoltaneKarim <karimsoltane92@gmail.com>
2024-05-07 17:51:11 +0100

    first step

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Contact from './components/Contact'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <Hero /> */}
+      <Contact />
+    </>
+  )
+}
+
+export default App
